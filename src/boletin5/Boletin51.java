32ef2b27f16ef30b424486fc9d25c9bd75a9fae4
dpazolopez <dpazolopez@eniac21i.danielcastelao.org>
2014-11-04 09:23:57 +0100

    finish

---
+++ src/boletin5/Boletin51.java
@@ -0,0 +1,11 @@
+package boletin5;
+// @author DAVID/
+public class Boletin51 {
+
+    public static void main(String[] args) {
+        Satelite satelite1 = new Satelite (21,82,37);
+        satelite1.verPosicion();
+       
+    }
+    
+}
